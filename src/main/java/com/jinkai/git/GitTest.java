be5dcce14875e5948e83c49fe098a4e8bf598cf6
jinkai <18302419198@163.com>
2023-03-20 20:59:26 +0800

    项目初始化，提交本地库

---
+++ src/main/java/com/jinkai/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.jinkai.git;
+
+/**
+ * @author jinkai
+ * @create 2023 年03 月20 日20:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
