99c693cdd91896ab7db4d02f1491b0cb84d3b601
jinkai <18302419198@163.com>
2023-03-20 21:06:44 +0800

    第二次提交

--- src/main/java/com/jinkai/git/GitTest.java
+++ src/main/java/com/jinkai/git/GitTest.java
@@ -7,5 +7,6 @@ package com.jinkai.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
     }
 }
